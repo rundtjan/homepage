e862843454e4eb1bcb53be4565ac8e7cc86d17cf
Jan Rundt <jan.rundt@gmail.com>
2020-06-09 16:29:33 +0300

    port

--- server.js
+++ server.js
@@ -7,4 +7,4 @@ var app = express();
 //Serves all the request which includes /images in the url from Images folder
 app.use(express.static(process.cwd()));
 
-var server = app.listen(8080);
+var server = app.listen(port);
